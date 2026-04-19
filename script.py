8738e88053443486e1d6a9e1f56512ddcb4b8797
Tropiqal <tropi1512@gmail.com>
2026-04-19 14:57:05 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,193 @@
+from tkinter import *
+from random import *
+
+x=randint(1, 4)
+
+a=Tk()
+a.title("The Flood Escape 2 Python Wheel")
+a.geometry("450x200")
+
+label1=Label(a, text="Remember: If you want another map, just run the program again!")
+label1.place(x=50, y=10)
+
+if x==1:
+    a1=Label(a, text="Easy", font=("Comic Sans MS", 30))
+    a1.place(x=50, y=30)
+    y=randint(1, 10)
+    if y==1:
+        b1=Label(a, text="Lapis Lagoon, ID: qzqzw*2")
+        b1.place(x=50, y=90)
+    if y==2:
+        b1=Label(a, text="The Poolrooms, ID: 54mr+g3")
+        b1.place(x=50, y=90)
+    if y==3:
+        b1=Label(a, text="Cloudy Skylands, ID: ed0ewk3")
+        b1.place(x=50, y=90)
+    if y==4:
+        b1=Label(a, text="Crawling Caverns, ID: kmc9173")
+        b1.place(x=50, y=90)
+    if y==5:
+        b1=Label(a, text="Outlier of a Coppice Carcass, ID: t8r*cs3")
+        b1.place(x=50, y=90)
+    if y==6:
+        b1=Label(a, text="Peaceful Plains, ID: bijszi3")
+        b1.place(x=50, y=90)
+    if y==7:
+        b1=Label(a, text="Sakura Falls, ID: 5majvo3")
+        b1.place(x=50, y=90)
+    if y==8:
+        b1=Label(a, text="Factory Center, ID: 8-ar3j1")
+        b1.place(x=50, y=90)
+    if y==9:
+        b1=Label(a, text="Autumn Vista, ID: an6i3t3")
+        b1.place(x=50, y=90)
+    if y==10:
+        b1=Label(a, text="Monsoon Forest, ID: ca04u13")
+        b1.place(x=50, y=90)
+
+
+if x==2:
+    y=randint(1, 14)
+    a1=Label(a, text="Normal", font=("Comic Sans MS", 30))
+    a1.place(x=50, y=30)
+    if y==1:
+        b1=Label(a, text="Harvest Falls, ID: ndsw4l3")
+        b1.place(x=50, y=90)
+    if y==2:
+        b1=Label(a, text="Wild Skatepark, ID: ID: ce-k1i3")
+        b1.place(x=50, y=90)
+    if y==3:
+        b1=Label(a, text="Sunsetter's Park, ID: n-98up1")
+        b1.place(x=50, y=90)
+    if y==4:
+        b1=Label(a, text="Unyielding Dusk, ID: l8kwtd3")
+        b1.place(x=50, y=90)
+    if y==5:
+        b1=Label(a, text="Frozen Fjord, ID: dl6nt53")
+        b1.place(x=50, y=90)
+    if y==6:
+        b1=Label(a, text="Acidic House, ID: bijszi3")
+        b1.place(x=50, y=90)
+    if y==7:
+        b1=Label(a, text="Poisonous Gorge, ID: +dczjp3")
+        b1.place(x=50, y=90)
+    if y==8:
+        b1=Label(a, text="Inverse Realm, ID: 58m+um3")
+        b1.place(x=50, y=90)
+    if y==9:
+        b1=Label(a, text="Lakeside Lumberyard, ID: p2-xq-2")
+        b1.place(x=50, y=90)
+    if y==10:
+        b1=Label(a, text="Nuclear Winter, ID: 2jpw7m3")
+        b1.place(x=50, y=90)
+    if y==11:
+        b1=Label(a, text="Frozen Fjord, ID: dl6nt53")
+        b1.place(x=50, y=90)
+    if y==12:
+        b1=Label(a, text="Mechanical Marsh, ID: -lzml34")
+        b1.place(x=50, y=90)
+    if y==13:
+        b1=Label(a, text="Hallowed Gorge, ID: j9gi/j3")
+        b1.place(x=50, y=90)
+    if y==14:
+        b1=Label(a, text="Pinstruck Venue, ID: +b92fd4")
+        b1.place(x=50, y=90)
+
+
+if x==3:
+    a1=Label(a, text="Hard", font=("Comic Sans MS", 30))
+    a1.place(x=50, y=30)
+    y=randint(1, 15)
+    if y==1:
+        b1=Label(a, text="Rainy Range, ID: 6m45n+2")
+        b1.place(x=50, y=90)
+    if y==2:
+        b1=Label(a, text="Wooded Abyss, ID: g/67tv3")
+        b1.place(x=50, y=90)
+    if y==3:
+        b1=Label(a, text="Oil Outpost, ID: r6is863")
+        b1.place(x=50, y=90)
+    if y==4:
+        b1=Label(a, text="Moonhaven, ID: w-hivu3")
+        b1.place(x=50, y=90)
+    if y==5:
+        b1=Label(a, text="Jungle Marsh, ID: +ep8j-2")
+        b1.place(x=50, y=90)
+    if y==6:
+        b1=Label(a, text="Firefly Vale, ID: 3yvfp+3")
+        b1.place(x=50, y=90)
+    if y==7:
+        b1=Label(a, text="Cascade Rift, ID: sq8klm3")
+        b1.place(x=50, y=90)
+    if y==8:
+        b1=Label(a, text="Sunken Library, ID: 1c0aw43")
+        b1.place(x=50, y=90)
+    if y==9:
+        b1=Label(a, text="Deserted Rainforest, ID: *h1*qy3")
+        b1.place(x=50, y=90)
+    if y==10:
+        b1=Label(a, text="Strawberry Springs, ID: jz/07g3")
+        b1.place(x=50, y=90)
+    if y==11:
+        b1=Label(a, text="Venomous Jungle, ID: s/8i3t3")
+        b1.place(x=50, y=90)
+    if y==12:
+        b1=Label(a, text="Cursed Crevasse, ID: /i0i694")
+        b1.place(x=50, y=90)
+    if y==13:
+        b1=Label(a, text="Hanami Highlands, ID: c8q5984")
+        b1.place(x=50, y=90)
+    if y==14:
+        b1=Label(a, text="Infiltration Outpost, ID: kausqh3")
+        b1.place(x=50, y=90)
+    if y==15:
+        b1=Label(a, text="The Great Wall of China, ID: nstokc4")
+        b1.place(x=50, y=90)
+
+
+if x==4:
+    y=randint(1, 14)
+    a1=Label(a, text="Insane", font=("Comic Sans MS", 30))
+    a1.place(x=50, y=30)
+    if y==1:
+        b1=Label(a, text="Ancient Golflands, ID: x8jj9m4")
+        b1.place(x=50, y=90)
+    if y==2:
+        b1=Label(a, text="Hallowed Vestige, ID: hsqqv33")
+        b1.place(x=50, y=90)
+    if y==3:
+        b1=Label(a, text="Distortion Crack, ID: ockrw53")
+        b1.place(x=50, y=90)
+    if y==4:
+        b1=Label(a, text="Boiling Point, ID: c8noiw3")
+        b1.place(x=50, y=90)
+    if y==5:
+        b1=Label(a, text="Autumn Views, ID: q*2dto3")
+        b1.place(x=50, y=90)
+    if y==6:
+        b1=Label(a, text="Rickety Outpost, ID: 3/z6gn3")
+        b1.place(x=50, y=90)
+    if y==7:
+        b1=Label(a, text="Muted Metro, ID: x8m+n23")
+        b1.place(x=50, y=90)
+    if y==8:
+        b1=Label(a, text="Wasteland Junction, ID: +ilzts3")
+        b1.place(x=50, y=90)
+    if y==9:
+        b1=Label(a, text="Cobalt Sanctuary, ID: 6o+roo3")
+        b1.place(x=50, y=90)
+    if y==10:
+        b1=Label(a, text="Power Tower, ID: x9zm0x3")
+        b1.place(x=50, y=90)
+    if y==11:
+        b1=Label(a, text="Perilous Sea, ID: /c6*ym3")
+        b1.place(x=50, y=90)
+    if y==12:
+        b1=Label(a, text="Desolate Urban, ID: -aosiq3")
+        b1.place(x=50, y=90)
+    if y==13:
+        b1=Label(a, text="Beach City, ID: c3+fml3")
+        b1.place(x=50, y=90)
+    if y==14:
+        b1=Label(a, text="Beep Block Station, ID: -p8by7+tap")
+        b1.place(x=50, y=90)
